178d1c4e24db3eea832d50a149b64003404b9023
Morozbea <morozbea@freemail.hu>
2017-01-21 23:30:49 +0100

    Start menu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+
+    public GameObject startButton;
+    public GameObject leaveGameButton;
+    // Use this for initialization
+    void Start()
+    {
+        startButton = GameObject.Find("ButtonStart");
+        leaveGameButton = GameObject.Find("ButtonLeave");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    public void StartGameFunction()
+    {
+        //if (Input.GetKeyDown(KeyCode.F1))
+        //{
+            SceneManager.LoadScene("level1");       
+
+        //}
+
+    }
+
+    public void LeaveGameFunction()
+    {        
+        Application.Quit();              
+    }
+
+}
