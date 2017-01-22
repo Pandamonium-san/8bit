00d224fa6c15d633abaa40fa512710fd30d20f60
Pandamonium-san <henrik.p95@gmail.com>
2017-01-22 12:51:20 +0100

    Add sounds to intro

--- Assets/Scripts/CatAnimation.cs
+++ Assets/Scripts/CatAnimation.cs
@@ -4,37 +4,51 @@ using UnityEngine;
 
 public class CatAnimation : MonoBehaviour
 {
-    float time;
-    Animator anim;
-    float timeToDissapear;
-    // Use this for initialization
-    void Start()
+  AudioSource m_AudioSource;
+  float time;
+  Animator anim;
+  float timeToDissapear;
+  // Use this for initialization
+  void Start()
+  {
+    time = 3.0f;
+    anim = this.GetComponent<Animator>();
+    timeToDissapear = 5f;
+
+    m_AudioSource = gameObject.AddComponent<AudioSource>();
+    m_AudioSource.spatialBlend = 0;
+    StartCoroutine(Sounds());
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    time -= Time.deltaTime;
+    if (time < 0 && !anim.GetBool("move"))
     {
-        time = 3.0f;
-        anim = this.GetComponent<Animator>();
-        timeToDissapear = 5f;
-       
+      anim.SetBool("move", true);
+      Debug.Log("StartCatAnim");
+
+    }
+    if (time < -2)
+    {
+      //this.gameObject.SetActive(false);
+      GetComponent<SpriteRenderer>().enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        time -= Time.deltaTime;
-        if (time < 0 && !anim.GetBool("move"))
-        {
-            anim.SetBool("move", true);
-            Debug.Log("StartCatAnim");
-            
-        }
-        if (time < -2)
-        {
-            this.gameObject.SetActive(false);
-        }
-        
-    }
+  }
 
-    public void Disappear()
-    {
-       
-    }
+  IEnumerator Sounds()
+  {
+    m_AudioSource.PlayOneShot((AudioClip)Resources.Load("Audio/sfx/shorewaves1"));
+    yield return new WaitForSeconds(6.0f);
+    m_AudioSource.PlayOneShot((AudioClip)Resources.Load("Audio/sfx/splash1"));
+    yield return new WaitForSeconds(1.5f);
+    m_AudioSource.PlayOneShot((AudioClip)Resources.Load("Audio/sfx/unplug1"));
+  }
+
+  public void Disappear()
+  {
+
+  }
 }
