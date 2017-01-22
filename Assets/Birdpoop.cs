2dd21768539d8753f7896dd22b8941a1974553d8
Pandamonium-san <henrik.p95@gmail.com>
2017-01-22 12:57:52 +0100

    Add upwards poo

--- Assets/Birdpoop.cs
+++ Assets/Birdpoop.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Birdpoop : MonoBehaviour {
+public class Birdpoop : MonoBehaviour
+{
 
   [SerializeField]
-  float lifeTime;
-	// Use this for initialization
-	void Start () {
-    GetComponent<Rigidbody2D>().gravityScale = transform.position.y > 0 ? 1 : -1;
+  float lifeTime = 2;
+  // Use this for initialization
+  void Start()
+  {
+    if (transform.position.y > 0)
+    {
+      GetComponent<Rigidbody2D>().gravityScale = 1;
+      transform.localScale = new Vector3(transform.localScale.x, 1);
+    }
+    else
+    {
+      GetComponent<Rigidbody2D>().gravityScale = -1;
+      transform.localScale = new Vector3(transform.localScale.x, -1);
+    }
   }
 
   // Update is called once per frame
-  void Update () {
+  void Update()
+  {
     lifeTime -= Time.deltaTime;
     if (lifeTime <= 0)
       Destroy(this.gameObject);
-	}
+  }
 }
