a7dc13f7898eaf6e1f56d61afa7e050ed704875d
Morozbea <morozbea@freemail.hu>
2017-01-22 08:56:48 +0100

    intro- island flies away

---
+++ Assets/Scripts/FlyingIslandScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingIslandScript : MonoBehaviour {
+
+    public GameObject island;
+    float time;
+	// Use this for initialization
+	void Start () {
+        island = GameObject.Find("FlyingIsland");
+        time = 5.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time -= Time.deltaTime;
+        if (time < 0)
+        {
+            island.transform.Translate(Vector2.up * Time.deltaTime, Space.World);
+        }
+	}
+
+
+}
