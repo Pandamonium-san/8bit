25d1a2d3479ef251409384caa422587d612bc631
Morozbea <morozbea@freemail.hu>
2017-01-22 12:19:33 +0100

    Intro lvel forts.

--- Assets/Scripts/CatAnimation.cs
+++ Assets/Scripts/CatAnimation.cs
@@ -2,16 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CatAnimation : MonoBehaviour {
+public class CatAnimation : MonoBehaviour
+{
+    float time;
+    Animator anim;
+    float timeToDissapear;
+    // Use this for initialization
+    void Start()
+    {
+        time = 3.0f;
+        anim = this.GetComponent<Animator>();
+        timeToDissapear = 5f;
+       
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time < 0 && !anim.GetBool("move"))
+        {
+            anim.SetBool("move", true);
+            Debug.Log("StartCatAnim");
+            
+        }
+        if (time < -2)
+        {
+            this.gameObject.SetActive(false);
+        }
+        
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void Disappear()
+    {
+       
+    }
 }
---
+++ Assets/Scripts/FromIntroToGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FromIntroToGame : MonoBehaviour {
+
+    float time;
+	// Use this for initialization
+	void Start () {
+        time = 12.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time -= Time.deltaTime;
+        if (time < 0)
+        {
+            ChangeScene();
+        }
+	}
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("level1");
+    }
+}
